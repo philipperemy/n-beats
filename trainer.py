e723f1aca6ded372f6f1f2113fbe9f760f0de856
philipperemy <premy@cogent.co.jp>
2019-07-24 10:32:51 +0900

    experimental

--- trainer.py
+++ trainer.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import torch
 from torch import optim
 from torch.nn import functional as F
@@ -12,14 +11,9 @@ def test(net, x, target, backcast_length, forecast_length, grad_step):
     net.eval()
     b, f = net(torch.tensor(x, dtype=torch.float))
     b, f, x, y = b.numpy()[0], f.numpy()[0], x[0], target[0]
-    x_y = np.concatenate([x, y], axis=-1).flatten()
     plt.plot(range(0, backcast_length), x, color='b')
     plt.plot(range(backcast_length, backcast_length + forecast_length), y, color='g')
     plt.plot(range(backcast_length, backcast_length + forecast_length), f, color='r')
-    # plt.scatter(range(0, backcast_length + forecast_length), x_y.flatten(),
-    #             color=['b'] * backcast_length + ['g'] * forecast_length)
-    # plt.scatter(range(backcast_length, backcast_length + forecast_length), f.flatten(),
-    #             color=['r'] * forecast_length)
     plt.grid(True)
     plt.title(grad_step)
     plt.show()
