162d318657c2ad8caad495e0b54696ac8e709431
Philippe Remy <premy@cogent.co.jp>
2019-11-10 19:03:33 +0900

    fix forward leaking data

--- data.py
+++ data.py
@@ -9,27 +9,40 @@ def get_data(num_samples, backcast_length, forecast_length, signal_type='seasona
         else:
             offset = 1
         if signal_type == 'trend':
-            x = lin_space + offset
+            a = lin_space + offset
         elif signal_type == 'seasonality':
-            x = np.cos(2 * np.random.randint(low=1, high=3) * np.pi * lin_space)
-            x += np.cos(2 * np.random.randint(low=2, high=4) * np.pi * lin_space)
-            x += lin_space * offset + np.random.rand() * 0.1
+            a = np.cos(2 * np.random.randint(low=1, high=3) * np.pi * lin_space)
+            a += np.cos(2 * np.random.randint(low=2, high=4) * np.pi * lin_space)
+            a += lin_space * offset + np.random.rand() * 0.1
         elif signal_type == 'cos':
-            x = np.cos(2 * np.pi * lin_space)
+            a = np.cos(2 * np.pi * lin_space)
         else:
             raise Exception('Unknown signal type.')
-        x -= np.minimum(np.min(x), 0)
-        x /= np.max(np.abs(x))
-        x = np.expand_dims(x, axis=0)
-        y = x[:, backcast_length:]
-        x = x[:, :backcast_length]
-        return x[0], y[0]
 
-    while True:
-        X = []
-        Y = []
-        for i in range(num_samples):
-            x, y = get_x_y()
-            X.append(x)
-            Y.append(y)
-        yield np.array(X), np.array(Y)
+        x = a[:backcast_length]
+        y = a[backcast_length:]
+
+        min_x, max_x = np.minimum(np.min(x), 0), np.max(np.abs(x))
+
+        x -= min_x
+        y -= min_x
+
+        x /= max_x
+        y /= max_x
+
+        return x, y
+
+    def gen():
+        while True:
+            xx = []
+            yy = []
+            for i in range(num_samples):
+                x, y = get_x_y()
+                xx.append(x)
+                yy.append(y)
+            yield np.array(xx), np.array(yy)
+
+    return gen()
+
+
+next(get_data(1000, 20, 10, signal_type='seasonality', random=False).__iter__())
