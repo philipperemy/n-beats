db573b88a5e2dfa3da959a0611b5fde34c26f8f5
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2020-07-31 18:58:21 +1000

    remove keras model summary and explain "exo" term

--- nbeats_keras/model.py
+++ nbeats_keras/model.py
@@ -77,11 +77,11 @@ class NBeatsNet:
         else:
             model = Model(x, y_)
 
-        model.summary()
-
         self.n_beats = model
 
     def has_exog(self):
+        # exo/exog is short for "exogenous variable", i.e. any input 
+        # features other than the target timeseries itself.
         return self.exo_dim > 0
 
     @staticmethod
